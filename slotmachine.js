a0e6e34e5769bbf0b5866f28d00d0049df770179
Alexander Collazo <alexander.collazo424@west-mec.org>
2016-11-14 15:11:15 -0700

    Update slotmachine.js

--- slotmachine.js
+++ slotmachine.js
@@ -1,23 +1,48 @@
+var credit = 50;
 function spin() {
-var color = ["image/suhdude.jpeg"
-            , "image/arthur.jpeg"
-            , "image/forever.png"
-            , "image/overhere.png"
-            , "image/wtf.jpg"
-            , "image/well.jpg"
-            , "image/harambe.jpeg"
-            , "image/fuuu.png"
-            , "image/cereal.png"
-            , "image/troll.png"];
+    var color = [
+          "image/sponge.gif"
+        , "image/shark.gif"
+        , "image/artist.gif"
+        , "image/patrick.gif"
+        , "image/neon.gif"
+        , "image/pacha.gif"
+        , "image/snoop.gif"
+        , "image/homer.gif"
+        , "image/blues.gif"
+        , "image/dance.gif"];
     var i1 = document.getElementById("pic1");
     var i2 = document.getElementById("pic2");
     var i3 = document.getElementById("pic3");
-//alert(i1.src);
+
     i1.src = color[Math.floor(Math.random() * 10)];
     i2.src = color[Math.floor(Math.random() * 10)];
     i3.src = color[Math.floor(Math.random() * 10)];
 
+    credit -= 1;
 
-//document.write(color[Math.floor(Math.random() * 9)]);
+    if (credit < 0) {
+        credit = 0;
+        i1.src = "image/gameover.png";
+        i2.src = "image/gameover.png";
+        i3.src = "image/gameover.png";
+    }
+
+    else if (i1.src == i2.src && i1.src == i3.src) {
+        credit += 15;
+        //alert("+25 CREDITS")
+    }
+
+    else if (i1.src == i2.src) {
+        credit += 5;
+        //alert("+5 CREDITS")
+    }
+
+    else if (i2.src == i3.src) {
+        credit += 5;
+        //alert("+5 CREDITS")
+    }
+    document.getElementById("tokenAmt").innerHTML = credit;
+
     return false;
 }
